a77e75dd353d8dcd679b5ee0121d26898faf6cd4
Egor Grishechko <egorgrishechko@gmail.com>
2018-06-08 23:30:17 +0300

    Add initial structure and valueTuples project

---
+++ Csharpunderhood/ValueTuples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ValueTuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = GetPersonalInfo();
+            Console.WriteLine($"Имя - {data.Item1}, возраст - {data.Item2}");
+
+            var namedData = GetPersonalInfoNamed();
+            Console.WriteLine($"Имя - {namedData.name}, возраст - {namedData.age}");
+
+            var (name, age) = GetPersonalInfo();
+        }
+
+        public static (string, int) GetPersonalInfo()
+        {
+            return ("Алиса", 123);
+        }
+
+        public static (string name, int age) GetPersonalInfoNamed()
+        {
+            return ("Алиса", 123);
+        }
+    }
+}
