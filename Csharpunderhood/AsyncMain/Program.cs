82a1f429af7dc3fc6e8b3b3c267f40f1085fa193
Egor Grishechko <egorgrishechko@gmail.com>
2018-06-08 23:45:23 +0300

    rename project. add second project

---
+++ Csharpunderhood/AsyncMain/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncMain
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
