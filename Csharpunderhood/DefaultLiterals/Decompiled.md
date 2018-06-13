26f529fa7c9dfcb4ac7c8b6d289d924f7bb025bc
Egor Grishechko <egorgrishechko@gmail.com>
2018-06-13 17:43:20 +0300

    default literals

---
+++ Csharpunderhood/DefaultLiterals/Decompiled.md
@@ -0,0 +1,15 @@
+﻿```csharp
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      int num1 = 0;
+      if (num1 == 0)
+        Console.WriteLine(string.Format("Maaagic {0}", (object) num1));
+      int num2 = 0;
+      if (num2 != 0)
+        return;
+      Console.WriteLine(string.Format("Maaagic #2 {0}", (object) num2));
+    }
+  }
+```
---
+++ Csharpunderhood/DefaultLiterals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DefaultLiterals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = default;
+            if(i == default) Console.WriteLine($"Maaagic {i}");
+
+            int oldDefault = default(int);
+            if (oldDefault == default(int)) Console.WriteLine($"Maaagic #2 {oldDefault}");
+        }
+    }
+}
